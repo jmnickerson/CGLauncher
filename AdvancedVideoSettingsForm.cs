f83a7bc6149744730942bc27c6eacbf4e7726de3
James McCabe Nickerson <jmccabenickerson@gmail.com>
2015-02-24 18:46:00 -0700

    Added Advanced Video Settings
    Added advanced video settings form.

---
+++ AdvancedVideoSettingsForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CryLaunch
+{
+    public partial class AdvancedVideoSettingsForm : Form
+    {
+        public AdvancedVideoSettingsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
