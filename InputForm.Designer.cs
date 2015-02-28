82bee71d820497866d9eda42bc29fcb19e3dc2a2
jmnickerson <trianglemanwins@gmail.com>
2015-02-28 11:44:53 -0700

    Added Input form basics

---
+++ InputForm.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace CryLaunch
+{
+    partial class InputForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.primaryActionLabel = new System.Windows.Forms.Label();
+            this.secondaryActionLabel = new System.Windows.Forms.Label();
+            this.primaryActionBox = new System.Windows.Forms.TextBox();
+            this.secondaryActionBox = new System.Windows.Forms.TextBox();
+            this.action1Box = new System.Windows.Forms.TextBox();
+            this.action6Box = new System.Windows.Forms.TextBox();
+            this.action3Box = new System.Windows.Forms.TextBox();
+            this.action4Box = new System.Windows.Forms.TextBox();
+            this.action2Box = new System.Windows.Forms.TextBox();
+            this.action5Box = new System.Windows.Forms.TextBox();
+            this.action1label = new System.Windows.Forms.Label();
+            this.action6label = new System.Windows.Forms.Label();
+            this.action5label = new System.Windows.Forms.Label();
+            this.action4label = new System.Windows.Forms.Label();
+            this.action3label = new System.Windows.Forms.Label();
+            this.action2label = new System.Windows.Forms.Label();
+            this.blockBox = new System.Windows.Forms.TextBox();
+            this.blocklabel = new System.Windows.Forms.Label();
+            this.dodgeBox = new System.Windows.Forms.TextBox();
+            this.dodgelabel = new System.Windows.Forms.Label();
+            this.jumpBox = new System.Windows.Forms.TextBox();
+            this.jumplabel = new System.Windows.Forms.Label();
+            this.closebutton = new System.Windows.Forms.Button();
+            this.cancelbutton = new System.Windows.Forms.Button();
+            this.applybutton = new System.Windows.Forms.Button();
+            this.interactlabel = new System.Windows.Forms.Label();
+            this.interactBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // primaryActionLabel
+            // 
+            this.primaryActionLabel.AutoSize = true;
+            this.primaryActionLabel.Location = new System.Drawing.Point(68, 34);
+            this.primaryActionLabel.Name = "primaryActionLabel";
+            this.primaryActionLabel.Size = new System.Drawing.Size(77, 13);
+            this.primaryActionLabel.TabIndex = 0;
+            this.primaryActionLabel.Text = "Primary Action:";
+            // 
+            // secondaryActionLabel
+            // 
+            this.secondaryActionLabel.AutoSize = true;
+            this.secondaryActionLabel.Location = new System.Drawing.Point(48, 60);
+            this.secondaryActionLabel.Name = "secondaryActionLabel";
+            this.secondaryActionLabel.Size = new System.Drawing.Size(94, 13);
+            this.secondaryActionLabel.TabIndex = 1;
+            this.secondaryActionLabel.Text = "Secondary Action:";
+            // 
+            // primaryActionBox
+            // 
+            this.primaryActionBox.Location = new System.Drawing.Point(146, 31);
+            this.primaryActionBox.Name = "primaryActionBox";
+            this.primaryActionBox.Size = new System.Drawing.Size(50, 20);
+            this.primaryActionBox.TabIndex = 2;
+            this.primaryActionBox.Tag = "";
+            this.primaryActionBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            //this.primaryActionBox.TextChanged += new System.EventHandler(this.primaryActionBox_TextChanged);
+            // 
+            // secondaryActionBox
+            // 
+            this.secondaryActionBox.Location = new System.Drawing.Point(146, 57);
+            this.secondaryActionBox.Name = "secondaryActionBox";
+            this.secondaryActionBox.Size = new System.Drawing.Size(50, 20);
+            this.secondaryActionBox.TabIndex = 3;
+            this.secondaryActionBox.Tag = "";
+            this.secondaryActionBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // action1Box
+            // 
+            this.action1Box.Location = new System.Drawing.Point(146, 83);
+            this.action1Box.Name = "action1Box";
+            this.action1Box.Size = new System.Drawing.Size(50, 20);
+            this.action1Box.TabIndex = 4;
+            this.action1Box.Tag = "";
+            this.action1Box.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.action1Box.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // action6Box
+            // 
+            this.action6Box.Location = new System.Drawing.Point(146, 213);
+            this.action6Box.Name = "action6Box";
+            this.action6Box.Size = new System.Drawing.Size(50, 20);
+            this.action6Box.TabIndex = 5;
+            this.action6Box.Tag = "";
+            this.action6Box.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // action3Box
+            // 
+            this.action3Box.Location = new System.Drawing.Point(146, 135);
+            this.action3Box.Name = "action3Box";
+            this.action3Box.Size = new System.Drawing.Size(50, 20);
+            this.action3Box.TabIndex = 6;
+            this.action3Box.Tag = "";
+            this.action3Box.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // action4Box
+            // 
+            this.action4Box.Location = new System.Drawing.Point(146, 161);
+            this.action4Box.Name = "action4Box";
+            this.action4Box.Size = new System.Drawing.Size(50, 20);
+            this.action4Box.TabIndex = 7;
+            this.action4Box.Tag = "";
+            this.action4Box.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // action2Box
+            // 
+            this.action2Box.Location = new System.Drawing.Point(146, 109);
+            this.action2Box.Name = "action2Box";
+            this.action2Box.Size = new System.Drawing.Size(50, 20);
+            this.action2Box.TabIndex = 8;
+            this.action2Box.Tag = "";
+            this.action2Box.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // action5Box
+            // 
+            this.action5Box.Location = new System.Drawing.Point(146, 187);
+            this.action5Box.Name = "action5Box";
+            this.action5Box.Size = new System.Drawing.Size(50, 20);
+            this.action5Box.TabIndex = 9;
+            this.action5Box.Tag = "";
+            this.action5Box.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // action1label
+            // 
+            this.action1label.AutoSize = true;
+            this.action1label.Location = new System.Drawing.Point(89, 86);
+            this.action1label.Name = "action1label";
+            this.action1label.Size = new System.Drawing.Size(49, 13);
+            this.action1label.TabIndex = 10;
+            this.action1label.Text = "Action 1:";
+            // 
+            // action6label
+            // 
+            this.action6label.AutoSize = true;
+            this.action6label.Location = new System.Drawing.Point(89, 220);
+            this.action6label.Name = "action6label";
+            this.action6label.Size = new System.Drawing.Size(49, 13);
+            this.action6label.TabIndex = 12;
+            this.action6label.Text = "Action 6:";
+            // 
+            // action5label
+            // 
+            this.action5label.AutoSize = true;
+            this.action5label.Location = new System.Drawing.Point(89, 194);
+            this.action5label.Name = "action5label";
+            this.action5label.Size = new System.Drawing.Size(49, 13);
+            this.action5label.TabIndex = 13;
+            this.action5label.Text = "Action 5:";
+            // 
+            // action4label
+            // 
+            this.action4label.AutoSize = true;
+            this.action4label.Location = new System.Drawing.Point(89, 164);
+            this.action4label.Name = "action4label";
+            this.action4label.Size = new System.Drawing.Size(49, 13);
+            this.action4label.TabIndex = 14;
+            this.action4label.Text = "Action 4:";
+            // 
+            // action3label
+            // 
+            this.action3label.AutoSize = true;
+            this.action3label.Location = new System.Drawing.Point(89, 138);
+            this.action3label.Name = "action3label";
+            this.action3label.Size = new System.Drawing.Size(49, 13);
+            this.action3label.TabIndex = 15;
+            this.action3label.Text = "Action 3:";
+            // 
+            // action2label
+            // 
+            this.action2label.AutoSize = true;
+            this.action2label.Location = new System.Drawing.Point(89, 112);
+            this.action2label.Name = "action2label";
+            this.action2label.Size = new System.Drawing.Size(49, 13);
+            this.action2label.TabIndex = 16;
+            this.action2label.Text = "Action 2:";
+            // 
+            // blockBox
+            // 
+            this.blockBox.Location = new System.Drawing.Point(146, 239);
+            this.blockBox.Name = "blockBox";
+            this.blockBox.Size = new System.Drawing.Size(50, 20);
+            this.blockBox.TabIndex = 17;
+            this.blockBox.Tag = "";
+            this.blockBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // blocklabel
+            // 
+            this.blocklabel.AutoSize = true;
+            this.blocklabel.Location = new System.Drawing.Point(101, 242);
+            this.blocklabel.Name = "blocklabel";
+            this.blocklabel.Size = new System.Drawing.Size(37, 13);
+            this.blocklabel.TabIndex = 18;
+            this.blocklabel.Text = "Block:";
+            // 
+            // dodgeBox
+            // 
+            this.dodgeBox.Location = new System.Drawing.Point(146, 265);
+            this.dodgeBox.Name = "dodgeBox";
+            this.dodgeBox.Size = new System.Drawing.Size(50, 20);
+            this.dodgeBox.TabIndex = 19;
+            this.dodgeBox.Tag = "";
+            this.dodgeBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // dodgelabel
+            // 
+            this.dodgelabel.AutoSize = true;
+            this.dodgelabel.Location = new System.Drawing.Point(97, 268);
+            this.dodgelabel.Name = "dodgelabel";
+            this.dodgelabel.Size = new System.Drawing.Size(45, 13);
+            this.dodgelabel.TabIndex = 20;
+            this.dodgelabel.Text = "Dodge: ";
+            // 
+            // jumpBox
+            // 
+            this.jumpBox.Location = new System.Drawing.Point(146, 291);
+            this.jumpBox.Name = "jumpBox";
+            this.jumpBox.Size = new System.Drawing.Size(50, 20);
+            this.jumpBox.TabIndex = 21;
+            this.jumpBox.Tag = "";
+            this.jumpBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // jumplabel
+            // 
+            this.jumplabel.AutoSize = true;
+            this.jumplabel.Location = new System.Drawing.Point(103, 294);
+            this.jumplabel.Name = "jumplabel";
+            this.jumplabel.Size = new System.Drawing.Size(35, 13);
+            this.jumplabel.TabIndex = 22;
+            this.jumplabel.Text = "Jump:";
+            this.jumplabel.Click += new System.EventHandler(this.jumplabel_Click);
+            // 
+            // closebutton
+            // 
+            this.closebutton.Location = new System.Drawing.Point(197, 372);
+            this.closebutton.Name = "closebutton";
+            this.closebutton.Size = new System.Drawing.Size(75, 23);
+            this.closebutton.TabIndex = 23;
+            this.closebutton.Text = "Close";
+            this.closebutton.UseVisualStyleBackColor = true;
+            this.closebutton.Click += new System.EventHandler(this.closebutton_Click);
+            // 
+            // cancelbutton
+            // 
+            this.cancelbutton.Location = new System.Drawing.Point(12, 372);
+            this.cancelbutton.Name = "cancelbutton";
+            this.cancelbutton.Size = new System.Drawing.Size(75, 23);
+            this.cancelbutton.TabIndex = 24;
+            this.cancelbutton.Text = "Cancel";
+            this.cancelbutton.UseVisualStyleBackColor = true;
+            this.cancelbutton.Click += new System.EventHandler(this.cancelbutton_Click);
+            // 
+            // applybutton
+            // 
+            this.applybutton.Location = new System.Drawing.Point(106, 372);
+            this.applybutton.Name = "applybutton";
+            this.applybutton.Size = new System.Drawing.Size(75, 23);
+            this.applybutton.TabIndex = 25;
+            this.applybutton.Text = "Apply";
+            this.applybutton.UseVisualStyleBackColor = true;
+            // 
+            // interactlabel
+            // 
+            this.interactlabel.AutoSize = true;
+            this.interactlabel.Location = new System.Drawing.Point(96, 320);
+            this.interactlabel.Name = "interactlabel";
+            this.interactlabel.Size = new System.Drawing.Size(46, 13);
+            this.interactlabel.TabIndex = 26;
+            this.interactlabel.Text = "Interact:";
+            // 
+            // interactBox
+            // 
+            this.interactBox.Location = new System.Drawing.Point(146, 317);
+            this.interactBox.Name = "interactBox";
+            this.interactBox.Size = new System.Drawing.Size(50, 20);
+            this.interactBox.TabIndex = 27;
+            this.interactBox.Tag = "";
+            this.interactBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // InputForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 411);
+            this.Controls.Add(this.interactBox);
+            this.Controls.Add(this.interactlabel);
+            this.Controls.Add(this.applybutton);
+            this.Controls.Add(this.cancelbutton);
+            this.Controls.Add(this.closebutton);
+            this.Controls.Add(this.jumplabel);
+            this.Controls.Add(this.jumpBox);
+            this.Controls.Add(this.dodgelabel);
+            this.Controls.Add(this.dodgeBox);
+            this.Controls.Add(this.blocklabel);
+            this.Controls.Add(this.blockBox);
+            this.Controls.Add(this.action2label);
+            this.Controls.Add(this.action3label);
+            this.Controls.Add(this.action4label);
+            this.Controls.Add(this.action5label);
+            this.Controls.Add(this.action6label);
+            this.Controls.Add(this.action1label);
+            this.Controls.Add(this.action5Box);
+            this.Controls.Add(this.action2Box);
+            this.Controls.Add(this.action4Box);
+            this.Controls.Add(this.action3Box);
+            this.Controls.Add(this.action6Box);
+            this.Controls.Add(this.action1Box);
+            this.Controls.Add(this.secondaryActionBox);
+            this.Controls.Add(this.primaryActionBox);
+            this.Controls.Add(this.secondaryActionLabel);
+            this.Controls.Add(this.primaryActionLabel);
+            this.Name = "InputForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Input Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label primaryActionLabel;
+        private System.Windows.Forms.Label secondaryActionLabel;
+        private System.Windows.Forms.TextBox primaryActionBox;
+        private System.Windows.Forms.TextBox secondaryActionBox;
+        private System.Windows.Forms.TextBox action1Box;
+        private System.Windows.Forms.TextBox action6Box;
+        private System.Windows.Forms.TextBox action3Box;
+        private System.Windows.Forms.TextBox action4Box;
+        private System.Windows.Forms.TextBox action2Box;
+        private System.Windows.Forms.TextBox action5Box;
+        private System.Windows.Forms.Label action1label;
+        private System.Windows.Forms.Label action6label;
+        private System.Windows.Forms.Label action5label;
+        private System.Windows.Forms.Label action4label;
+        private System.Windows.Forms.Label action3label;
+        private System.Windows.Forms.Label action2label;
+        private System.Windows.Forms.TextBox blockBox;
+        private System.Windows.Forms.Label blocklabel;
+        private System.Windows.Forms.TextBox dodgeBox;
+        private System.Windows.Forms.Label dodgelabel;
+        private System.Windows.Forms.TextBox jumpBox;
+        private System.Windows.Forms.Label jumplabel;
+        private System.Windows.Forms.Button closebutton;
+        private System.Windows.Forms.Button cancelbutton;
+        private System.Windows.Forms.Button applybutton;
+        private System.Windows.Forms.Label interactlabel;
+        private System.Windows.Forms.TextBox interactBox;
+    }
+}
--- VideoSettings.cs
+++ VideoSettings.cs
@@ -10,7 +10,7 @@ namespace CryLaunch
 {
     class VideoSettings
     {
-		private string FILE = "";
+        private string FILE = "E:/Dropbox/Dropbox/Programming/CryLaunch/autoexec2.cfg";
         private bool fullScreen;
         private bool VSync;
         private bool motionBlur;
