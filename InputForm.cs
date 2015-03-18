4d06a6a924c0e83eebda5af6b00ca25dd24c968c
James McCabe Nickerson <jmccabenickerson@gmail.com>
2015-03-18 08:42:03 -0600

    XML Reading
    Added reading the profile.xml.

--- InputForm.cs
+++ InputForm.cs
@@ -15,9 +15,29 @@ namespace CGLauncher
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         static extern bool HideCaret(IntPtr hWnd);
         private ContextMenu blankMenu = new ContextMenu();
+        private InputSettings inputSettings;
         public InputForm()
         {
             InitializeComponent();
+            inputSettings = new InputSettings();
+            setFromFile();
+        }
+
+        private void setFromFile()
+        {
+            System.Collections.Generic.SortedDictionary<string, string> keybinds = inputSettings.getKeybinds();
+            string val;
+
+            if (keybinds.TryGetValue("click1", out val))
+            {
+                primaryActionBox.Text = "Mouse 1";
+            }
+            if (keybinds.TryGetValue("click2", out val))
+            {
+                secondaryActionBox.Text = "Mouse 2";
+            }
+            
+ 
         }
 
         private void cancelbutton_Click(object sender, EventArgs e)
@@ -27,6 +47,7 @@ namespace CGLauncher
 
         private void closebutton_Click(object sender, EventArgs e)
         {
+            outputFile();
             this.Close();
         }
 
@@ -52,12 +73,19 @@ namespace CGLauncher
             TextBox source = sender as TextBox;
             if (e.Button == MouseButtons.Left)
             {
-                source.Text = "Left Mouse";
+                source.Text = "Mouse 1";
             }
             if (e.Button == MouseButtons.Right)
             {
-                source.Text = "Right Mouse";
+                source.Text = "Mouse 2";
             }
         }
+
+        private void outputFile()
+        {
+
+        }
+
+
     }
 }
--- InputSettings.cs
+++ InputSettings.cs
@@ -2,10 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Xml;
+using System.IO;
 namespace CGLauncher
 {
     class InputSettings
     {
+        private string FILE = "c:/defaultProfile.xml";
+        private System.Collections.Generic.SortedDictionary<string, string> keybinds;
+        public InputSettings()
+        {
+            keybinds = new SortedDictionary<string, string>();
+            readFile();
+        }
+
+        public System.Collections.Generic.SortedDictionary<string, string> getKeybinds()
+        {
+            return keybinds;
+        }
+
+        private void readFile()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(FILE);
+            XmlElement root = xmlDoc.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("//actionmap/action");
+
+            foreach (XmlNode xmlNode in nodes)
+            {
+                try
+                {
+                    string name = xmlNode.Attributes["name"].Value;
+                    string key = xmlNode.Attributes["keyboard"].Value;
+                    //Console.WriteLine("Name: " + name + " Keyboard: " + key);
+                    keybinds.Add(name, key);
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        }
     }
 }
